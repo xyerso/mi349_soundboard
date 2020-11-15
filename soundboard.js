a77ba426388fe55769a4b138fbd0a417c90f79bb
xyerso <Xyerso@163.com>
2020-11-15 14:48:09 +0800

    first commit

---
+++ soundboard.js
@@ -0,0 +1,18 @@
+
+ var a = document.getElementById("alarm1");
+
+ var c = document.getElementById("carhorn1");
+
+ var o = document.getElementById("ocean1");
+
+function playAudio() {
+    a.play();
+}
+
+function playAudio() {
+   c.play();
+}
+
+function playAudio() {
+    o.play();
+}
